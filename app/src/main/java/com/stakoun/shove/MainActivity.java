982f5d5138b6ada178f79000bb7f51ce2dbc9efe
Peter Stakoun <peter@stakoun.com>
2016-05-18 20:32:11 -0400

    Add initialization for error TextView

--- app/src/main/java/com/stakoun/shove/MainActivity.java
+++ app/src/main/java/com/stakoun/shove/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.error_message)).setText("");
     }
 
     private void error(String message)
